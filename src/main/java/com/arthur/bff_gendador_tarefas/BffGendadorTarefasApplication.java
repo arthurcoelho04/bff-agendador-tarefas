592a3bd7d05c5742a6da6a1e666dd3929c438136
Arthur <arthurcoelhom04@gmail.com>
2026-05-09 17:32:37 -0300

    adicionado esqueleto projeto

---
+++ src/main/java/com/arthur/bff_gendador_tarefas/BffGendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.bff_gendador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffGendadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffGendadorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthur/bff_gendador_tarefas/BffGendadorTarefasApplicationTests.java
@@ -0,0 +1,10 @@
+package com.arthur.bff_gendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffGendadorTarefasApplicationTests {
+
+
+}
